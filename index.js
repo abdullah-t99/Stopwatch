c9bf115c26e479a7e654804f525e23c17e69d331
abdullah-t99 <60823411+abdullah-t99@users.noreply.github.com>
2023-02-15 23:18:15 +0000

    Additional Work

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
 window.onload = function () {
   var minutes = 00;
   var seconds = 00;
-  var tens = 00;
-  var appendTens = document.getElementById("tens");
-  var appendSeconds = document.getElementById("seconds");
-  var buttonStart = document.getElementById("button-start");
-  var buttonStop = document.getElementById("button-stop");
-  var buttonReset = document.getElementById("button-reset");
+  var milliseconds = 00;
+  var guiSeconds = document.getElementById("seconds");
+  var guiMilliseconds = document.getElementById("milliseconds");
+  var buttonStart = document.getElementById("start-button");
+  var buttonStop = document.getElementById("stop-button");
+  var buttonRestart = document.getElementById("restart-button");
   var Interval;
 };
+
+function startTimer() {
+  milliseconds++;
+
+  if (milliseconds <= 9) {
+    guiMilliseconds.innerHTML = "0" + milliseconds;
+  }
+
+  if (milliseconds > 9) {
+    guiMilliseconds.innerHTML = milliseconds;
+  }
+
+  if (milliseconds > 99) {
+    console.log("seconds");
+    seconds++;
+    guiSeconds.innerHTML = "0" + seconds;
+    milliseconds = 0;
+    guiMilliseconds.innerHTML = "0" + 0;
+  }
+
+  if (seconds > 9) {
+    appendSeconds.innerHTML = seconds;
+  }
+}
